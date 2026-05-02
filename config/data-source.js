9903f66086c7b2fc8edfd037dbd9122193633493
HaiderAliBurney <haiderburney0@gmail.com>
2026-05-02 18:27:54 +0500

    Requirement 2: Sync database passwords for CI

--- config/data-source.js
+++ config/data-source.js
@@ -11,7 +11,7 @@ const AppDataSource = new DataSource({
   host: process.env.DB_HOST || "localhost",
   port: Number(process.env.DB_PORT) || 5432,
   username: process.env.DB_USERNAME || "postgres",
-  password: "Haider@123", // Cast to String to prevent the SASL error
+  password: process.env.DB_PASSWORD, // Cast to String to prevent the SASL error
   database: process.env.DB_NAME || "Assignment-2-db",
   synchronize: true,
   logging: false,
